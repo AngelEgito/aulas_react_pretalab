c3550652ed896b6236668202cd3147d2510f4980
angelegito <angelegito@hotmail.com>
2022-11-10 21:36:31 -0300

    Subindo as aulas

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Formulario from './componentes/Formulario'
+
+function App() {
+  return (
+    <div className="app">
+      <Formulario />
+    </div>
+  )
+}
+
+export default App
---
+++ src/componentes/Titulo.jsx
@@ -0,0 +1,7 @@
+function Titulo() {
+    return (
+        <h1>Olá, eu sou um componente</h1>
+    )
+}
+
+export default Titulo
